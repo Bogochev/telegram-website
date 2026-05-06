1c9d569dd7b43972d4cf67e4d486484087aa4bd0
Gary <jindalgaurav1998@gmail.com>
2026-05-06 13:55:48 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+const cors = require('cors');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use(express.static(__dirname));
+
+// YOUR TELEGRAM BOT TOKEN
+const BOT_TOKEN = '8639768633:AAECOGGd5DDDSNgb8lpPZfodZAL7GZaxWPY';
+
+// YOUR TELEGRAM CHAT ID
+const CHAT_ID = '8715498548';
+
+app.post('/submit', async (req, res) => {
+    try {
+        const {
+            name,
+            phone,
+            email,
+            message
+        } = req.body;
+
+        const text = `
+🔥 New Lead Received
+
+👤 Name: ${name}
+📞 Phone: ${phone}
+📧 Email: ${email}
+💬 Message: ${message}
+        `;
+
+        const telegramURL = `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`;
+
+        await axios.post(telegramURL, {
+            chat_id: CHAT_ID,
+            text: text
+        });
+
+        res.json({
+            success: true,
+            message: 'Form submitted successfully'
+        });
+
+    } catch (error) {
+        console.log(error);
+
+        res.status(500).json({
+            success: false,
+            message: 'Error submitting form'
+        });
+    }
+});
+
+app.listen(3000, () => {
+    console.log('Server running on http://localhost:3000');
+});
